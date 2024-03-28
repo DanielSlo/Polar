aaad339a7a8035b0f77119d29c053b82d24c0059
Daniel Sloseris <danielsloseris@d207-023-216-253.wireless.sfu.ca>
2024-03-28 13:37:14 -0700

    recommendations get adde tp queue after search

--- Spotify API project python/recommendations.py
+++ Spotify API project python/recommendations.py
@@ -2,6 +2,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import streamlit as st
+from datetime import datetime
+import time
 
 scope = ["user-library-read", "user-modify-playback-state", 
          "user-top-read", "user-read-recently-played", "user-read-playback-state"]
@@ -18,12 +20,17 @@ def get_recommendations(track_name):
     recommendations = sp.recommendations(seed_tracks=[track_uri], limit=num_recommendations)['tracks']
     return recommendations
 
+# Define a function to display the digital clock
+def digital_clock():
+    current_time = datetime.now().strftime("%H:%M:%S")
+    st.write(f"The current time is: {current_time}")
 
 
 
 #generate web app
 st.title("Music Recommendation System")
 track_name = st.text_input("Enter a song name:")
+
 
 #if user inputs a track name display 5 recommendations based on that song and 
 #play top recommendation on availble device
@@ -49,6 +56,11 @@ if track_name:
     if len(devices) >= 1 and devices['devices'][0]['is_active']:
         sp.start_playback(uris=[uri])
         sp.repeat(state="off")
+        sp.shuffle(state=True)
+        for track in recommendations[1:]:
+            sp.add_to_queue(track['uri'])
+
+        
     else:
         print("No available devices to start playback")
 
